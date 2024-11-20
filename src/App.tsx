ef36e7e373b88d5a4f279c0d2de335e0b9ed2a6a
MehdiKhoshnevisz <mkhnsz1996@gmail.com>
2024-11-20 13:06:19 +0330

    Create dynamic post route

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 
 import Home from "@/pages/home";
+import PostPage from "@/pages/post";
 import NotFound from "@/pages/not-found";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/post/:id" element={<PostPage />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
