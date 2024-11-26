1a4fa935cf12e017e51b6d35149c25bf01ced855
MehdiKhoshnevisz <mkhnsz1996@gmail.com>
2024-11-27 00:01:28 +0330

    Add motion animation

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,17 @@
+import { motion } from "motion/react";
+
 import Biography from "./components/Biography";
 import Posts from "./components/Posts";
 
 export default function Home() {
   return (
-    <main>
+    <motion.main
+      initial={{ opacity: 0, y: 30 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5, ease: "easeOut" }}
+    >
       <Biography />
       <Posts />
-    </main>
+    </motion.main>
   );
 }
