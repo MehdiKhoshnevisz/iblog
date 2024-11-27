af2a56f3b4a28ec40c6563aeb35e7307720ab64e
MehdiKhoshnevisz <mkhnsz1996@gmail.com>
2024-11-27 21:06:06 +0330

    Add auth token into env variables

--- src/services/apollo-client.ts
+++ src/services/apollo-client.ts
@@ -4,13 +4,11 @@ import {
   NormalizedCacheObject,
 } from "@apollo/client";
 
-const token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ik5jMDlFOHN1eTMiLCJuZXR3b3JrSWQiOiJTWm5Zb0E3RmFQIiwibmV0d29ya0RvbWFpbiI6ImJhc2ljLTNzdWdtZHhyLmJldHRlcm1vZGUuaW8iLCJ0b2tlblR5cGUiOiJVU0VSIiwiZW50aXR5SWQiOm51bGwsInBlcm1pc3Npb25Db250ZXh0IjpudWxsLCJwZXJtaXNzaW9ucyI6bnVsbCwic2Vzc2lvbklkIjoiQk9QSlJzTzJGaVpsMFN2aHJmV1NKbnNsb3A4ck02OTRYTkVRSzBNakEwVWI0a3EwWjQiLCJpYXQiOjE3MzIyNzU4NjIsImV4cCI6MTczNDg2Nzg2Mn0.pNHKsecDQ1JF3NZ6P7aSi7S72098jpiRx1yskTEITig`;
-
 const apolloClient = new ApolloClient<NormalizedCacheObject>({
   uri: import.meta.env.VITE_BASE_API as string,
   cache: new InMemoryCache(),
   headers: {
-    Authorization: `Bearer ${token}`,
+    Authorization: `Bearer ${import.meta.env.VITE_AUTH_TOKEN as string}`,
   },
 });
 
