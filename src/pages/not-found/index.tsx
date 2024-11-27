b13fd28e386427b8d355f13847ce3749d3793de3
MehdiKhoshnevisz <mkhnsz1996@gmail.com>
2024-11-27 20:44:02 +0330

    Update not found page

--- src/pages/not-found/index.tsx
+++ src/pages/not-found/index.tsx
@@ -1,3 +1,20 @@
+import { Link } from "react-router-dom";
+
+import Container from "@/components/Container";
+
 export default function NotFound() {
-  return <>not found!</>;
+  return (
+    <Container className="min-h-screen flex justify-center items-center">
+      <div className="text-center">
+        <p className="text-4xl mb-8">Opps! Nothing is here :|</p>
+        {/* TODO: shoud be a component */}
+        <Link
+          to="/"
+          className="min-h-9 text-center px-4 py-2 text-slate-950 font-black bg-slate-100 hover:bg-slate-950 hover:text-slate-50 transition-all duration-300"
+        >
+          Go to home
+        </Link>
+      </div>
+    </Container>
+  );
 }
